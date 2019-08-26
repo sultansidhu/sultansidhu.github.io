221e3282ace8204c14159cd6402103bcef6c1eae
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-08-25 21:17:55 -0400

    font fixes for responsive web design

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+
+
 $(function(){
     
+    
+    var OSName = "Unknown";
+    if (window.navigator.userAgent.indexOf("Windows NT 10.0")!= -1) OSName="Windows 10";
+    if (window.navigator.userAgent.indexOf("Windows NT 6.2") != -1) OSName="Windows 8";
+    if (window.navigator.userAgent.indexOf("Windows NT 6.1") != -1) OSName="Windows 7";
+    if (window.navigator.userAgent.indexOf("Windows NT 6.0") != -1) OSName="Windows Vista";
+    if (window.navigator.userAgent.indexOf("Windows NT 5.1") != -1) OSName="Windows XP";
+    if (window.navigator.userAgent.indexOf("Windows NT 5.0") != -1) OSName="Windows 2000";
+    if (window.navigator.userAgent.indexOf("Mac")            != -1) OSName="Mac/iOS";
+    if (window.navigator.userAgent.indexOf("X11")            != -1) OSName="UNIX";
+    if (window.navigator.userAgent.indexOf("Linux")          != -1) OSName="Linux";
+    
+    if (OSName != "Mac/iOS"){
+        //avenir 
+        $('header, #bioScreen, #skillScreen, #experienceScreen, #projectScreen, #educationScreen, #contactScreen').css('font-family', 'Avenir Next');
+    }
     $('.index-btn').click(function() {
         $('.active').removeClass('active');
         var clickedIndex = $('.index-btn').index($(this));
@@ -132,5 +150,5 @@ $(function(){
     
 });
 
-console.log(navigator.userAgent);
+
 
